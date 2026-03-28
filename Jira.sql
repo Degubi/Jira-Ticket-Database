4b90b25fbf85a522a03dade001545d9486b69823
Degubi <gergonoorbi@gmail.com>
2026-03-28 11:16:09 +0100

    Switch to storing only minutes in DB

--- Jira.sql
+++ Jira.sql
@@ -1,12 +1,10 @@
 CREATE TABLE Issues (
     key TEXT PRIMARY KEY,
-    hours INTEGER DEFAULT 0,
     minutes INTEGER DEFAULT 0
 );
 
 CREATE TABLE IssueUpdates (
     id INTEGER PRIMARY KEY,
     key TEXT,
-    hoursChange INTEGER,
-    minutesChange INTEGER
+    minutes INTEGER
 );
--- src/main.rs
+++ src/main.rs
@@ -33,16 +33,15 @@ fn main() {
 
     println!("Updated tickets:");
 
-    db_connection.prepare("SELECT key, SUM(hoursChange), SUM(minutesChange) FROM IssueUpdates GROUP BY key").unwrap()
+    db_connection.prepare("SELECT key, SUM(minutes) FROM IssueUpdates GROUP BY key").unwrap()
                  .query_map([], |row| Ok(DatabaseIssue {
                     key: row.get(0).unwrap(),
-                    hours: row.get(1).unwrap(),
-                    minutes: row.get(2).unwrap()
+                    minutes: row.get(1).unwrap()
                  })).unwrap()
                  .map(Result::unwrap)
-                 .for_each(|k| println!("{}: {}h {}m", k.key, k.hours, k.minutes));
+                 .for_each(|k| println!("{}: {}h {}m", k.key, k.minutes / 60, k.minutes % 60));
 
-    let total_update_minutes = db_connection.prepare("SELECT coalesce(SUM(hoursChange * 60 + minutesChange), 0) FROM IssueUpdates").unwrap()
+    let total_update_minutes = db_connection.prepare("SELECT coalesce(SUM(minutes), 0) FROM IssueUpdates").unwrap()
                                             .query_one([], |row| Ok(row.get::<_, i32>(0))).unwrap()
                                             .unwrap();
 
@@ -50,71 +49,59 @@ fn main() {
 }
 
 fn process_jira_issue(jira_issue: &JiraIssue, db_connection: &Connection, time_regex: &Regex) {
-    let db_issue_stats = db_connection.prepare_cached("SELECT key, hours, minutes FROM Issues WHERE key = ?1").unwrap()
+    let db_issue_stats = db_connection.prepare_cached("SELECT key, minutes FROM Issues WHERE key = ?1").unwrap()
                                       .query_one([ &jira_issue.key ], |row| Ok(DatabaseIssue {
                                           key: row.get(0).unwrap(),
-                                          hours: row.get(1).unwrap(),
-                                          minutes: row.get(2).unwrap()
+                                          minutes: row.get(1).unwrap()
                                       })).ok();
     match db_issue_stats {
-        Some(DatabaseIssue { key: issue_key, hours: db_issue_hours, minutes: db_issue_minutes }) => {
-            let jira_time_parts = parse_time(jira_issue.fields.timetracking.timeSpent.as_ref().map(String::as_str).unwrap_or(""), time_regex);
+        Some(DatabaseIssue { key: issue_key, minutes: db_minutes }) => {
+            let jira_minutes = parse_timestamp_into_minutes(jira_issue.fields.timetracking.timeSpent.as_ref().map(String::as_str).unwrap_or(""), time_regex);
 
-            match (jira_time_parts, (db_issue_hours, db_issue_minutes)) {
-                ((0, 0), (0, 0)) => {},
-                ((0, 0), (_, _)) => panic!("This should never happen!"),
-                (jira_time, (0, 0)) => handle_issue_with_newly_set_time(&issue_key, jira_time, db_connection),
-                (jira_time, db_time) if jira_time == db_time => {},
-                (jira_time, db_time) => handle_issue_with_time_change(&issue_key, jira_time, db_time, db_connection)
+            match (jira_minutes, db_minutes) {
+                (0, 0) => {},
+                (0, _) => panic!("This should never happen!"),
+                (jira_minutes, 0) => handle_issue_with_newly_set_time(&issue_key, jira_minutes, db_connection),
+                (jira_minutes, db_minutes) if jira_minutes == db_minutes => {},
+                (jira_minutes, db_minutes) => handle_issue_with_time_change(&issue_key, jira_minutes, db_minutes, db_connection)
             }
         },
         None => handle_issue_not_in_db(jira_issue, db_connection, time_regex)
     }
 }
 
-fn handle_issue_with_time_change(issue_key: &str, (jira_hours, jira_minutes): (i32, i32), (db_hours, db_minutes): (i32, i32), db_connection: &Connection) {
-    if read_from_console(&format!("Issue '{issue_key}' previously had {}h {}m time in db, jira has {}h {}m. Update it in DB? (y = yes, n = no)", db_hours, db_minutes, jira_hours, jira_minutes)) == "y" {
-        db_connection.execute("UPDATE Issues SET hours = ?2, minutes = ?3 WHERE key = ?1", params![ issue_key, jira_hours, jira_minutes ]).unwrap();
-
-        let (hour_updates, minute_updates) = db_connection.prepare_cached("SELECT coalesce(SUM(hoursChange), 0), coalesce(SUM(minutesChange), 0) FROM IssueUpdates WHERE key = ?1").unwrap()
-                                                          .query_one([ issue_key ], |row| Ok((row.get::<_, i32>(0).unwrap(), row.get::<_, i32>(1).unwrap()))).unwrap();
-
-        let old_total_minutes = db_hours * 60 + db_minutes;
-        let new_total_minutes = jira_hours * 60 + jira_minutes;
-        let updates_total_minutes = hour_updates * 60 + minute_updates;
-        let total_diff_minutes = new_total_minutes - updates_total_minutes - old_total_minutes;
-        let params = params![ &issue_key, total_diff_minutes / 60, total_diff_minutes % 60 ];
-
-        db_connection.execute("INSERT INTO IssueUpdates(key, hoursChange, minutesChange) VALUES(?1, ?2, ?3)", params).unwrap();
+fn handle_issue_with_time_change(issue_key: &str, jira_minutes: i32, db_minutes: i32, db_connection: &Connection) {
+    if read_from_console(&format!("Issue '{issue_key}' previously had {}h {}m time in db, jira has {}h {}m. Update it in DB? (y = yes, n = no)", db_minutes / 60, db_minutes % 60, jira_minutes / 60, jira_minutes % 60)) == "y" {
+        db_connection.execute("UPDATE Issues SET minutes = ?2 WHERE key = ?1", params![ issue_key, jira_minutes ]).unwrap();
+        db_connection.execute("INSERT INTO IssueUpdates(key, minutes) VALUES(?1, ?2)", params![ issue_key, jira_minutes - db_minutes ]).unwrap();
     }
 }
 
-fn handle_issue_with_newly_set_time(issue_key: &str, (jira_minutes, jira_hours): (i32, i32), db_connection: &Connection) {
-    if read_from_console(&format!("Issue '{issue_key}' had no time in db, received {}h {}m. Update it in DB? (y = yes, n = no)", jira_hours, jira_minutes)) == "y" {
-        let params = params![ issue_key, jira_hours, jira_minutes ];
+fn handle_issue_with_newly_set_time(issue_key: &str, jira_minutes: i32, db_connection: &Connection) {
+    if read_from_console(&format!("Issue '{issue_key}' had no time in db, received {}h {}m. Update it in DB? (y = yes, n = no)", jira_minutes / 60, jira_minutes % 60)) == "y" {
+        let params = params![ issue_key, jira_minutes ];
 
-        db_connection.execute("UPDATE Issues SET hours = ?2, minutes = ?3 WHERE key = ?1", params).unwrap();
-        db_connection.execute("INSERT INTO IssueUpdates(key, hoursChange, minutesChange) VALUES(?1, ?2, ?3)", params).unwrap();
+        db_connection.execute("UPDATE Issues SET minutes = ?2 WHERE key = ?1", params).unwrap();
+        db_connection.execute("INSERT INTO IssueUpdates(key, minutes) VALUES(?1, ?2)", params).unwrap();
     }
 }
 
 fn handle_issue_not_in_db(issue: &JiraIssue, db_connection: &Connection, time_regex: &Regex) {
     if read_from_console(&format!("Found new issue: {}, add it to DB? (y = yes, n = no)", issue.key)) == "y" {
-        let (hours, minutes) = parse_time(issue.fields.timetracking.timeSpent.as_ref().map(String::as_str).unwrap_or(""), time_regex);
-        let params = params![ &issue.key, hours, minutes ];
+        let minutes = parse_timestamp_into_minutes(issue.fields.timetracking.timeSpent.as_ref().map(String::as_str).unwrap_or(""), time_regex);
+        let params = params![ &issue.key, minutes ];
 
-        db_connection.execute("INSERT INTO Issues(key, hours, minutes) VALUES(?1, ?2, ?3)", params).unwrap();
-        db_connection.execute("INSERT INTO IssueUpdates(key, hoursChange, minutesChange) VALUES(?1, ?2, ?3)", params).unwrap();
+        db_connection.execute("INSERT INTO Issues(key, minutes) VALUES(?1, ?2)", params).unwrap();
+        db_connection.execute("INSERT INTO IssueUpdates(key, minutes) VALUES(?1, ?2)", params).unwrap();
     }
 }
 
-fn parse_time(time_str: &str, time_regex: &Regex) -> (i32, i32) {
+fn parse_timestamp_into_minutes(time_str: &str, time_regex: &Regex) -> i32 {
     match time_regex.captures(time_str) {
-        Some(k) => (
-            k.name("days").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0) * 8 + k.name("hours").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0),
-            k.name("minutes").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0)
-        ),
-        None => (0, 0)
+        Some(k) => (k.name("days").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0) * 8 * 60) +
+                   (k.name("hours").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0) * 60) +
+                    k.name("minutes").map(|l| l.as_str().parse::<i32>().unwrap()).unwrap_or(0),
+        None => 0
     }
 }
 
@@ -151,7 +138,6 @@ struct JiraTimeTracking {
 
 struct DatabaseIssue {
     key: String,
-    hours: i32,
     minutes: i32
 }
 
